f925ccf3b0324504eb67a46408f1bc51e68963bc
sho11 <sho11@HS-PC>
2023-02-20 21:56:09 +0900

    Chapter3完了　

---
+++ Exercise3_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+
+// 3.1.1
+int result1 = numbers.Count(n => (n % 8 == 0) || (n % 9 == 0));
+Console.WriteLine(result1);
+
+// 3.1.2
+numbers.ForEach(n => Console.WriteLine(n / 2.0));
+
+// 3.1.3
+List<int> result3 = numbers.Where(n => n >= 50).ToList();
+result3.ForEach(n => Console.WriteLine(n));
+
+// 3.1.4
+List<int> result4 = numbers.Select(n => n * 2).ToList();
+result4.ForEach(n => Console.WriteLine(n));
---
+++ Exercise3_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var names = new List<string>
+{
+    "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+};
+
+// 3.2.1
+var line = Console.ReadLine();
+var result1 = names.FindIndex(n => n == line);
+Console.WriteLine(result1);
+
+// 3.2.2
+var result2 = names.Count(n => n.Contains('o'));
+Console.WriteLine(result2);
+
+// 3.2.3
+var results3 = names.Where(n => n.Contains('o')).ToArray();
+foreach(var result3 in results3)
+{
+    Console.WriteLine(result3);
+}
+
+// 3.2.4
+var results4 = names.Where(n => n[0] == 'B').Select(n => n.Length);
+foreach (var result4 in results4)
+{
+    Console.WriteLine(result4);
+}
