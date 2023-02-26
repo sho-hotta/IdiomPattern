2c22a5bca3b47a9167ea6510582017eb678d9222
sho11 <sho1127.db@gmail.com>
2023-02-26 16:03:49 +0900

    Chapter4完了

---
+++ Exercise4_1/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Exercise4;
+
+// 4.2.1
+var yearMonthArray = new YearMonth[]
+{
+    new YearMonth(1995, 11),
+    new YearMonth(1998, 2),
+    new YearMonth(2023, 2),
+    new YearMonth(1999, 12),
+    new YearMonth(2000, 1)
+};
+
+// 4.2.2
+foreach (var yearMonth in yearMonthArray)
+{
+    Console.Write($"{yearMonth.ToString()} ");
+}
+Console.WriteLine();
+
+// 4.2.4
+YearMonth first21CenturyYear = Search21Century(yearMonthArray);
+if (first21CenturyYear == null)
+{
+    Console.WriteLine($"21世紀のデータはありません");
+}
+else
+{
+    Console.WriteLine($"{first21CenturyYear.ToString()}");
+}
+
+// 4.2.3
+YearMonth Search21Century(YearMonth[] yearMonthsArray)
+{
+    foreach (var yearMonth in yearMonthArray)
+    {
+        if (yearMonth.Is21Century)
+        {
+            return yearMonth;
+        }
+    }
+    return null;
+}
+
+// 4.2.5
+YearMonth[] oneMonthLaterArray = yearMonthArray.Select(n => n.AddOneMonth()).ToArray();
+foreach (var yearMonth in oneMonthLaterArray)
+{
+    Console.Write($"{yearMonth.ToString()} ");
+}
---
+++ Exercise4_1/YearMonth.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    public class YearMonth
+    {
+        public int Year { get; private set; }
+        public int Month { get; private set; }
+        public bool Is21Century => (2001 <= Year && Year <= 2100);
+
+        public YearMonth(int year, int month)
+        {
+            Year = year;
+            Month = month;
+        }
+
+        public YearMonth AddOneMonth()
+        {
+            int year;
+            int month;
+
+            if (Month == 12) {
+                year = Year + 1;
+                month = 1;
+            }
+            else
+            {
+                year = Year;
+                month = Month + 1;
+            }
+
+            var oneMonthLater = new YearMonth(year, month);
+            return oneMonthLater;
+        }
+
+        public override string ToString()
+        {
+            return $"{Year}年{Month}月";
+        }
+    }
+}
